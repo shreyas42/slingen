791f2d78f3c943c2aa20f8a13ac9dbfa4996a630
shreyas42 <ravi@spiral_gpu.andrew.cmu.edu>
2018-07-03 17:50:18 -0400

    Removed libs

--- libs/include/isl/stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ISL_INCLUDE_ISL_STDINT_H
-#define _ISL_INCLUDE_ISL_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "isl 0.12.1"
-/* generated using gnu compiler gcc (GCC) 6.4.1 20170727 (Red Hat 6.4.1-1) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
